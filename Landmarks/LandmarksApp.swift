9d5ef2f9175b2d6e67a7a8840a04635565355c92
Harshit Garg <04harshitgarg@gmail.com>
2024-08-14 13:05:17 +0530

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Harshit Garg on 14/08/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
